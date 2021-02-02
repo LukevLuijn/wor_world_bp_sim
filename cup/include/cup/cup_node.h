f10eb57a0e497bcbcbcdff0ce97e2e24886c084b
Luke van Luijn <LWH.vanluijn@student.han.nl>
2021-02-02 22:50:20 +0100

    [bp-sim] multiple additions - Added doxygen to header. - Added parsing capability to main. - Added functionality for the cup to be placed anywhere. - Added debug and error log messages to code. - Removed some unnecessary code.
    Signed-off-by: Luke van Luijn <LWH.vanluijn@student.han.nl>

--- cup/include/cup/cup_node.h
+++ cup/include/cup/cup_node.h
@@ -4,46 +4,26 @@
 
 #ifndef CLIONPROJECT_CUP_NODE_H
 #define CLIONPROJECT_CUP_NODE_H
+
 /** CUP REQUIREMENTS
  *
- * TODO random location.
- *  >   pose position arg in main.
- *
- * TODO publish 3D object.
- *  >   maker and some stuff, all done.
- *
- * TODO detect hit fields of gripper.
- *  >   arm publish gripper point, cup -> read.
- *
- * TODO visual effect when gripper holds cup.
- *  >   change color of cup
- *
- * TODO move with gripper (when held).
- *  >   arm publish gripper loc, cup -> read.
- *
- * TODO fall when released.
- *  >   fall straight down with speed.
- *
- * TODO calc location and publish this.
- *  >   easy
- *
- * TODO calc velocity and publish this.
- *  >   hard
- *
- * TODO something with RQT plot.
- *  >   no idea
- *
+ * DONE random location.                        > pose position arg in main.
+ * DONE publish 3D object.                      > maker and some stuff, all done.
+ * TODO detect hit fields of gripper.           > arm publish gripper point, cup -> read.
+ * TODO visual effect when gripper holds cup.   > change color of cup
+ * TODO move with gripper (when held).          > arm publish gripper loc, cup -> read.
+ * TODO fall when released.                     > fall straight down with speed.
+ * DONE calc location and publish this.         > easy
+ * TODO calc velocity and publish this.         > hard
+ * TODO something with RQT plot.                > no idea
  */
 
-/// ROS HEADERS
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/time.hpp"
 
-/// TF2
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 
-/// MESSAGES
 #include <visualization_msgs/msg/marker.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
@@ -54,71 +34,113 @@ namespace States
 {
 enum CupState
 {
-  MOVING = 0,
-  FALLING = 1,
-  STILL = 2
+  MOVING = 0,   /// state when the cup is moving.
+  FALLING = 1,  /// state when the cup is falling.
+  STILL = 2     /// state when the cup is standing still.
 };
 }
 
 class CupNode : public rclcpp::Node
 {
-  typedef States::CupState State;
-
-  typedef visualization_msgs::msg::Marker Marker_msg;
-
-  typedef geometry_msgs::msg::TransformStamped Trans_stamped_msg;
-  typedef geometry_msgs::msg::Quaternion Quaternion_msg;
-  typedef geometry_msgs::msg::Twist Twist_msg;
-  typedef geometry_msgs::msg::Pose Pose_msg;
-  typedef geometry_msgs::msg::Point Point_msg;
-
-  typedef std_msgs::msg::ColorRGBA Color_msg;
+  typedef States::CupState State;                                  /// typedef cupstate
+  typedef visualization_msgs::msg::Marker Marker_msg;              /// typedef maker message
+  typedef geometry_msgs::msg::TransformStamped Trans_stamped_msg;  /// typedef transform stamped message
+  typedef geometry_msgs::msg::Twist Twist_msg;                     /// typedef twist message
+  typedef geometry_msgs::msg::Point Point_msg;                     /// typedef point message
+  typedef std_msgs::msg::ColorRGBA Color_msg;                      /// typedef color message
+  typedef geometry_msgs::msg::Pose Pose_msg;                       /// typedef pose message
 
 public:
+  /**
+   * Constructor of the CupNode class.
+   *
+   * @param frame   name of the global frame.
+   * @param aPoint  start location of the cup.
+   */
   explicit CupNode(std::string frame, Point_msg aPoint);
+
+  /**
+   * Destructor of the CupNode Class.
+   */
   ~CupNode() override = default;
 
 private:
+  /**
+   * Timer callback function. calls publishers and broadcaster every 10ms
+   */
   void timerCallback();
 
+  /**
+   * broadcaster which broadcasts the transform. TODO
+   */
+  void broadcastTransform();
+
+  /**
+   * updates and publishes the marker message.
+   */
   void publishMarker();
-  void publishPose();
+
+  /**
+   * updates and publishes the twist message.
+   */
   void publishTwist();
 
+  /**
+   * updates and publishes the pose message
+   */
+  void publishPose();
+
+  /**
+   * Sets color of the marker according to the current state of the cup.
+   *
+   * @param msg reference to color attribute of the message.
+   */
   void setCupColor(Color_msg &msg);
+
+  /**
+   * Sets the pose of the marker.
+   *
+   * @param msg reference to pose attribute of the message.
+   */
   void setCupPose(Pose_msg &msg);
 
-private:  /// initial setup
+  /**
+   * calls the different message initializers.
+   *
+   * @param init_point class param containing the start location.
+   */
   void initMessages(Point_msg init_point);
-  void initPose(Point_msg init_point);
+
+  /**
+   * Initializer of the marker message, sets the base values.
+   *
+   * @param init_point class param containing the start location.
+   */
+  void initMarker(Point_msg init_point);
+
+  /**
+   * Initializer of the transform stamped message, sets the base values.
+   */
   void initTransform();
-  void initMarker();
 
-private:  /// fields
-  State current_state_ = State::STILL;
+private:
+  State current_state_ = State::STILL;                   /// current state of cup
+  rclcpp::TimerBase::SharedPtr timer_;                   /// wall timer (10 ms)
+  tf2_ros::Buffer buffer_;                               /// tf buffer
+  tf2_ros::TransformListener trans_listener_;            /// transform listener
+  tf2_ros::TransformBroadcaster trans_broadcaster_;      /// transform broadcaster
+  Trans_stamped_msg trans_stamped_msg_;                  /// transform stamped message
+  Marker_msg marker_msg_;                                /// marker message
+  Twist_msg twist_msg_;                                  /// twist message
+  std::string header_frame_id_ = "n/a";                  /// parent frame id
+  rclcpp::Publisher<Marker_msg>::SharedPtr marker_pub_;  /// publisher for marker message
+  rclcpp::Publisher<Twist_msg>::SharedPtr twist_pub_;    /// publisher for twist message
+  rclcpp::Publisher<Pose_msg>::SharedPtr pose_pub_;      /// publisher for pose message
 
-  rclcpp::TimerBase::SharedPtr timer_;
-
-  tf2_ros::Buffer buffer_;
-  tf2_ros::TransformListener trans_listener_;
-  tf2_ros::TransformBroadcaster trans_broadcaster_;
-
-  rclcpp::Publisher<Marker_msg>::SharedPtr marker_pub_;
-  rclcpp::Publisher<Twist_msg>::SharedPtr twist_pub_;
-  rclcpp::Publisher<Pose_msg>::SharedPtr pose_pub_;
-
-  Trans_stamped_msg trans_stamped_msg_;
-  Marker_msg marker_msg_;
-  Twist_msg twist_msg_;
-  Pose_msg pose_msg_;
-
-  std::string header_frame_id_ = "n/a";
-
-private:  /// const fields
-  const rclcpp::QoS QOS_VAL = 10;
-  const std::string MESSAGE_NAMESPACE = "simulation/cup/";
-  const std::string FRAME_ID = "cup_link";
-  const std::string RESOURCE_PATH = "package://cup/resource/cup.stl";  // TODO
+  const rclcpp::QoS QOS_VAL = 10;                           /// Quality of service value for published messages
+  const std::string MESSAGE_NAMESPACE = "simulation/cup/";  /// namespace for marker message
+  const std::string FRAME_ID = "cup_link";                  /// frame id for cup
+  const std::string RESOURCE_PATH = "package://cup/resource/cup.stl";  /// resource location
 };
 
 #endif  // CLIONPROJECT_CUP_NODE_H
--- cup/src/cup_node.cpp
+++ cup/src/cup_node.cpp
@@ -5,26 +5,6 @@
 
 using namespace std::chrono_literals;
 
-int main(int argc, char* argv[])
-{
-  // TODO read from exe agrs.
-  geometry_msgs::msg::Point point;
-  point.x = 0;  // should be given
-  point.y = 0;  // should be given
-  point.z = 0;
-
-  std::string frame_id = "world_link";
-  // end
-
-  if (argc > 0)  // ros2 run cup cup_node --ros-args -- <arg>
-    RCLCPP_INFO(rclcpp::get_logger("test"), argv[3]);
-
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<CupNode>(frame_id, point));
-  rclcpp::shutdown();
-  return 0;
-}
-
 CupNode::CupNode(std::string frame, Point_msg aPoint)
   : Node("cup")
   , buffer_(get_clock())
@@ -32,41 +12,76 @@ CupNode::CupNode(std::string frame, Point_msg aPoint)
   , trans_broadcaster_(this)
   , header_frame_id_(std::move(frame))
 {
-  initMessages(aPoint);
+  RCLCPP_DEBUG(this->get_logger(), "initialization started..");
 
-  timer_ = create_wall_timer(500ms, [this] { timerCallback(); });
+  initMessages(aPoint);
+  RCLCPP_DEBUG(this->get_logger(), "messages created");
+
+  timer_ = create_wall_timer(10ms, [this] { timerCallback(); });
 
   marker_pub_ = create_publisher<Marker_msg>(MESSAGE_NAMESPACE + "marker", QOS_VAL);
   twist_pub_ = create_publisher<Twist_msg>(MESSAGE_NAMESPACE + "twist", QOS_VAL);
   pose_pub_ = create_publisher<Pose_msg>(MESSAGE_NAMESPACE + "pose", QOS_VAL);
+  RCLCPP_DEBUG(this->get_logger(), "publishers created");
+
+  /// TODO subscribers
+
+  RCLCPP_DEBUG(this->get_logger(), "subscribers created");
+
+  RCLCPP_DEBUG(this->get_logger(), "initialization complete");
 }
 
 void CupNode::timerCallback()
 {
+  broadcastTransform();
   publishMarker();
   publishTwist();
   publishPose();
 }
 
+void CupNode::broadcastTransform()
+{
+  // TODO check things
+  try
+  {
+    geometry_msgs::msg::TransformStamped transformStamped;
+    transformStamped = buffer_.lookupTransform(header_frame_id_, FRAME_ID, rclcpp::Time(0));
+
+    trans_stamped_msg_.transform = transformStamped.transform;
+    trans_stamped_msg_.header.frame_id = header_frame_id_;
+  }
+  catch (tf2::TransformException& ex)
+  {
+    RCLCPP_ERROR(this->get_logger(), ex.what());
+  }
+
+  trans_broadcaster_.sendTransform(trans_stamped_msg_);
+}
+
 void CupNode::publishMarker()
 {
+  marker_msg_.header.stamp = now();
+  marker_msg_.header.frame_id = FRAME_ID;
+
   setCupColor(marker_msg_.color);
   setCupPose(marker_msg_.pose);
-
   marker_pub_->publish(marker_msg_);
 }
 
 void CupNode::publishTwist()
 {
+  // TODO
 }
 void CupNode::publishPose()
 {
+  setCupPose(marker_msg_.pose);
+  pose_pub_->publish(marker_msg_.pose);
 }
 void CupNode::setCupColor(CupNode::Color_msg& msg)
 {
-  const std::vector<std::vector<float>> COLORS{ { 1.0f, 0.0f, 0.0f, 1.0f },
-                                                { 1.0f, 0.0f, 0.0f, 1.0f },
-                                                { 1.0f, 0.0f, 0.0f, 1.0f } };
+  const std::vector<std::vector<float>> COLORS{ { 1.0f, 0.0f, 0.0f, 1.0f },    // MOVING   [RED]
+                                                { 1.0f, 1.0f, 0.0f, 1.0f },    // FALLING  [YELLOW]
+                                                { 0.0f, 1.0f, 0.0f, 1.0f } };  // STILL    [GREEN]
   msg.r = COLORS.at(current_state_).at(0);
   msg.g = COLORS.at(current_state_).at(1);
   msg.b = COLORS.at(current_state_).at(2);
@@ -74,53 +89,15 @@ void CupNode::setCupColor(CupNode::Color_msg& msg)
 }
 void CupNode::setCupPose(CupNode::Pose_msg& msg)
 {
-  msg.position.x += 0.001;  // TODO
 }
 
 void CupNode::initMessages(CupNode::Point_msg init_point)
 {
-  initPose(init_point);
+  initMarker(init_point);
   initTransform();
-  initMarker();
 }
 
-void CupNode::initPose(CupNode::Point_msg init_point)
-{
-  // create pose message
-  pose_msg_ = Pose_msg();
-
-  // set position to given position
-  pose_msg_.position = init_point;
-
-  // set orientation
-  pose_msg_.orientation.x = 0;
-  pose_msg_.orientation.y = 0;
-  pose_msg_.orientation.z = 0;
-  pose_msg_.orientation.w = 1;
-}
-
-void CupNode::initTransform()
-{
-  // create trans stamped message
-  trans_stamped_msg_ = Trans_stamped_msg();
-
-  // set header values
-  trans_stamped_msg_.header.stamp = now();
-  trans_stamped_msg_.header.frame_id = header_frame_id_;
-
-  // set child frame
-  trans_stamped_msg_.child_frame_id = FRAME_ID;
-
-  // set rotation
-  trans_stamped_msg_.transform.rotation = pose_msg_.orientation;
-
-  // set translation
-  trans_stamped_msg_.transform.translation.x = pose_msg_.position.x;
-  trans_stamped_msg_.transform.translation.y = pose_msg_.position.y;
-  trans_stamped_msg_.transform.translation.z = pose_msg_.position.z;
-}
-
-void CupNode::initMarker()
+void CupNode::initMarker(Point_msg init_point)
 {
   // create marker message
   marker_msg_ = Marker_msg();
@@ -128,6 +105,15 @@ void CupNode::initMarker()
   // set header values
   marker_msg_.header.stamp = now();
   marker_msg_.header.frame_id = FRAME_ID;
+
+  // set position
+  marker_msg_.pose.position = init_point;
+
+  // set orientation
+  marker_msg_.pose.orientation.x = 0;
+  marker_msg_.pose.orientation.y = 0;
+  marker_msg_.pose.orientation.z = 0;
+  marker_msg_.pose.orientation.w = 1;
 
   // set resource
   marker_msg_.type = visualization_msgs::msg::Marker::MESH_RESOURCE;
@@ -143,8 +129,84 @@ void CupNode::initMarker()
 
   // set color and pose
   setCupColor(marker_msg_.color);
-  setCupPose(marker_msg_.pose);
 
   // set lifetime
   marker_msg_.lifetime = builtin_interfaces::msg::Duration();
+}
+
+void CupNode::initTransform()
+{
+  // create trans stamped message
+  trans_stamped_msg_ = Trans_stamped_msg();
+
+  // set header values
+  trans_stamped_msg_.header.stamp = now();
+  trans_stamped_msg_.header.frame_id = header_frame_id_;
+
+  // set child frame
+  trans_stamped_msg_.child_frame_id = FRAME_ID;
+
+  // set rotation
+  trans_stamped_msg_.transform.rotation = marker_msg_.pose.orientation;
+
+  // set translation
+  trans_stamped_msg_.transform.translation.x = marker_msg_.pose.position.x;
+  trans_stamped_msg_.transform.translation.y = marker_msg_.pose.position.y;
+  trans_stamped_msg_.transform.translation.z = marker_msg_.pose.position.z;
+
+  // initial broadcast
+  trans_broadcaster_.sendTransform(trans_stamped_msg_);
+}
+
+////////
+////////    MAIN
+////////
+
+signed int castToInt(const std::string& arg, const std::string& name)
+{
+  signed int val = 0;
+
+  try
+  {
+    val = std::stoi(arg);
+    std::max(val, -5);
+    std::min(val, 5);
+  }
+  catch (std::invalid_argument& ex)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("launch"),
+                 "invalid arguments for cup location [" + name + "], location set to default.");
+  }
+  catch (std::logic_error& ex)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("launch"), "logic error for cup location [" + name + "], location set to default.");
+  }
+
+  return val;
+}
+
+geometry_msgs::msg::Point parseLocation(int argc, char* argv[])
+{
+  geometry_msgs::msg::Point point;
+  point.x = 0;  // should be given
+  point.y = 0;  // should be given
+  point.z = 0;
+
+  if (argc < 5) return point;
+
+  point.x = castToInt(argv[4], "x");
+  point.y = castToInt(argv[5], "y");
+
+  return point;
+}
+
+int main(int argc, char* argv[])
+{
+  std::string frame_id = (argc > 3) ? argv[3] : "world_link";
+  geometry_msgs::msg::Point point = parseLocation(argc, argv);
+
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CupNode>(frame_id, point));
+  rclcpp::shutdown();
+  return 0;
 }
